ff98642984a8969d6a52362cd040432b46c53e11
Quantum <quantum2048@gmail.com>
2020-05-05 16:22:18 -0400

    Use explicit HTTPS for gravatar

--- judge/jinja2/gravatar.py
+++ judge/jinja2/gravatar.py
@@ -17,7 +17,11 @@ def gravatar(email, size=80, default=None):
     elif isinstance(email, AbstractUser):
         email = email.email
 
+<<<<<<< HEAD
     gravatar_url = 'https://gravatar.loli.net/avatar/' + hashlib.md5(utf8bytes(email.strip().lower())).hexdigest() + '?'
+=======
+    gravatar_url = 'https://www.gravatar.com/avatar/' + hashlib.md5(utf8bytes(email.strip().lower())).hexdigest() + '?'
+>>>>>>> Use explicit HTTPS for gravatar
     args = {'d': 'identicon', 's': str(size)}
     if default:
         args['f'] = 'y'
