88e775ec9f685dc0c2ff4766b4490bf4b4b2180b
Outvi V <git@outv.im>
2020-12-15 21:58:18 +0800

    feat: add azure ad auth

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -536,6 +536,7 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
     'social_core.backends.facebook.FacebookOAuth2',
     'judge.social_auth.GitHubSecureEmailOAuth2',
+    'social_core.backends.azuread.AzureADOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
 
